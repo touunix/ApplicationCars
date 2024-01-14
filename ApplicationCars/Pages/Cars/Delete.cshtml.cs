5488dd64696314f531d4bd318912c5e7d1ee94d4
touunix <touunix@gmail.com>
2024-01-14 15:58:02 +0100

    Add data model

---
+++ ApplicationCars/Pages/Cars/Delete.cshtml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ApplicationCars.Data;
+using ApplicationCars.Models;
+
+namespace ApplicationCars.Pages.Cars
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly ApplicationCars.Data.ApplicationCarsContext _context;
+
+        public DeleteModel(ApplicationCars.Data.ApplicationCarsContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+      public Car Car { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Car == null)
+            {
+                return NotFound();
+            }
+
+            var car = await _context.Car.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (car == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Car = car;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null || _context.Car == null)
+            {
+                return NotFound();
+            }
+            var car = await _context.Car.FindAsync(id);
+
+            if (car != null)
+            {
+                Car = car;
+                _context.Car.Remove(Car);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ ApplicationCars/Pages/Cars/Edit.cshtml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ApplicationCars.Data;
+using ApplicationCars.Models;
+
+namespace ApplicationCars.Pages.Cars
+{
+    public class EditModel : PageModel
+    {
+        private readonly ApplicationCars.Data.ApplicationCarsContext _context;
+
+        public EditModel(ApplicationCars.Data.ApplicationCarsContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Car Car { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Car == null)
+            {
+                return NotFound();
+            }
+
+            var car =  await _context.Car.FirstOrDefaultAsync(m => m.Id == id);
+            if (car == null)
+            {
+                return NotFound();
+            }
+            Car = car;
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Car).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CarExists(Car.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool CarExists(int id)
+        {
+          return (_context.Car?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
