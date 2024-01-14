fdb1a703554071174ad946d753791f4882f3ef2e
touunix <touunix@gmail.com>
2024-01-14 18:48:13 +0100

    Add validation

--- ApplicationCars/Migrations/20240114174706_New_DataAnnotations.Designer.cs
+++ ApplicationCars/Migrations/20240114174706_New_DataAnnotations.Designer.cs
@@ -4,6 +4,7 @@ using ApplicationCars.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ApplicationCars.Migrations
 {
     [DbContext(typeof(ApplicationCarsContext))]
-    partial class ApplicationCarsContextModelSnapshot : ModelSnapshot
+    [Migration("20240114174706_New_DataAnnotations")]
+    partial class New_DataAnnotations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- ApplicationCars/Models/Car.cs
+++ ApplicationCars/Models/Car.cs
@@ -5,7 +5,11 @@ namespace ApplicationCars.Models;
 public class Car
 {
     public int Id { get; set; }
+
+    [StringLength(60, MinimumLength = 3)]
+    [Required]
     public string? Brand { get; set; }
+
     public string? Model { get; set; }
     public decimal Price { get; set; }
 
